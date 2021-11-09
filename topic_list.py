7bc9e5b907c760f01a5a2614c63f10faff705299
Mahmoud-Yasser-16 <mahmoud.yasser@corporatica.com>
2021-11-09 22:27:09 +0200

    cleaning my the

--- topic_list.py
+++ topic_list.py
@@ -5,7 +5,7 @@ import nltk
 import spacy
 import time 
 import re
-from forbidden import forbidden_words as forbidden
+from forbidden import forbidden_words 
 
 stop_words = set(stopwords.words('english'))
 nlp = spacy.load("en_core_web_sm")
@@ -18,11 +18,11 @@ def extract_topic(body_text, subreddit,spaCy=False,debug=False):
     body_text=re.sub(r"""\-""", '', body_text, flags=re.MULTILINE)
     body_text=re.sub(r"""\|""", '', body_text, flags=re.MULTILINE)
     body_text=re.sub(r"""\s\s+""", ' ', body_text, flags=re.MULTILINE)
-
+    body_text=" "+body_text+" "
     if not spaCy:
         start = time.time()    
         word_tokens_body = word_tokenize(body_text)
-        filtered_sentence = [w+"_"+subreddit for w in word_tokens_body if (not w.lower() in stop_words) and (w.lower() not in forbidden)]
+        filtered_sentence = [w+"_"+subreddit for w in word_tokens_body if (not w.lower() in stop_words) and (w.lower() not in forbidden_words)]
         if debug:
             print("time taken for NLTK:")
             print(time.time()-start)
@@ -30,7 +30,14 @@ def extract_topic(body_text, subreddit,spaCy=False,debug=False):
     
     start = time.time()    
     processed_body = nlp(body_text)
-    nouns = [ str(noun).lower()+"_sep_"+subreddit for noun in processed_body.noun_chunks if str(noun).lower() not in forbidden]
+    nouns = [ " "+str(noun).lower()+" " for noun in processed_body.noun_chunks if " "+str(noun).lower()+" " not in forbidden_words]
+    for n in range(len(nouns)):
+        for word in forbidden_words:
+            nouns[n]=nouns[n].replace(word,"")
+        nouns[n]=nouns[n].strip()
+        nouns[n]=re.sub(r"""\s\s+""", ' ', nouns[n], flags=re.MULTILINE)
+        nouns[n]+="_sep_"+subreddit
+
     if debug:
         print("time taken for spaCy:")
         print(time.time()-start)
