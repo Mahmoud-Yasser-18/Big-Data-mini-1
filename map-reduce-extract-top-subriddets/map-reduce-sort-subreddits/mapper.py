03506527f0b82c8178f4c4779d56615b99b1f656
ِAsmar097 <>
2021-11-10 00:55:09 +0200

    Added another sorting job and organized files respectively

---
+++ map-reduce-extract-top-subriddets/map-reduce-sort-subreddits/mapper.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import os
+import json
+import sys
+
+# Notes :
+# 1. All Down votes are 0
+# 2. All Controversiality are 0
+
+
+# Option or NLP: NLTK, spaCy
+
+
+for line in sys.stdin:
+    j_i=json.loads(line)
+    try:
+        print(j_i["subreddit"],1,sep="\t")
+    except:
+        continue
+    
+
+
