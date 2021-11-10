bfa8590654c1ce8d5c2755e9bb075fa77150db67
ِAsmar097 <>
2021-11-10 17:31:00 +0200

    Added topic count in top subreddits job

--- mp-extract-top-subreddits/mp-topic-count-in-subreddits/mapper.py
+++ mp-extract-top-subreddits/mp-topic-count-in-subreddits/mapper.py
@@ -10,16 +10,15 @@ from topic_list import extract_topic
 
 # Option or NLP: NLTK, spaCy
 
-
+f = open("./top_subreddits.txt", "r")
+top_subreddits=[line.split('\t')[1].replace('/n','') for line in f.readlines() if line != '/t/n']
 for line in sys.stdin:
     j_i=json.loads(line)
-    topics = extract_topic(j_i["body"],j_i["subreddit"],spaCy=True)
-    for topic in topics:
-        print(topic,1,sep="\t")
-    # print("j_i[ups]",j_i["ups"])
-    # print("j_i[downs]",j_i["downs"])
-    # print("j_i[score]",j_i["score"])
-    # print(json.dumps(j_i, indent=4))
+    if j_i["subreddit"] in top_subreddits :
+        topics = extract_topic(j_i["body"],j_i["subreddit"],spaCy=True)
+        for topic in topics:
+            print(topic,1,sep="\t")
+
 
 
 
