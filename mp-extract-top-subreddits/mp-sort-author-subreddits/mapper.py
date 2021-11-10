0147acd517df3625587eb2845a1b6f5a2d47744b
Mahmoud-Yasser-16 <mahmoud.yasser@corporatica.com>
2021-11-10 16:20:13 +0200

    down top users sorted

--- mp-extract-top-subreddits/mp-sort-author-subreddits/mapper.py
+++ mp-extract-top-subreddits/mp-sort-author-subreddits/mapper.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
 import sys
+import json
 
 for line in sys.stdin:
+    if line.split("\t")[0].split("_sep_")[0].replace("\n","")=="[deleted]":
+        continue
     print(format(int(line.strip().split('\t')[1]), '032b').replace('0','b').replace('1','a'),line.strip().split('\t')[0],sep = "\t")
     
     
