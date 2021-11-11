5576f9cda254f29bd1b10b1e31d32dc181e16989
Mahmoud-Yasser-16 <mahmoud.yasser@corporatica.com>
2021-11-11 23:31:07 +0200

    fix first sorter mapper and reducer

--- Task1/mp-subreddits-sort/mapper.py
+++ Task1/mp-subreddits-sort/mapper.py
@@ -3,8 +3,4 @@
 import sys
 
 for line in sys.stdin:
-    print(format(int(line.strip().split('\t')[1]), '032b').replace('0','b').replace('1','a'),line.strip().split('\t')[0],sep = "\t")
-    
-    
-
-
+    print(line)
--- Task1/mp-subreddits-sort/reducer.py
+++ Task1/mp-subreddits-sort/reducer.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python3
 
 import sys
-
+import bisect
 word = None
 count = 0
-
+top_records=[]
 for line in sys.stdin:
-    print(line)
-    count += 1
-    if count == 10 :
-        break
+    try:
+        key, value = line.strip().split(sep='\t')
+        value=int(value)
+    except:
+        continue
+    try:
+        if value > top_records[0][0]:
+            top_records.append((value,key))
+            top_records=sorted(top_records)[-10:]
+    except:
+        top_records.append((value,key))
+
+for record in top_records:
+    print(record[1],record[0],sep='\t')
