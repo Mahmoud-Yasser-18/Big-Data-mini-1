b4443c25116762ff9c829376c94cb57f122b39c6
Mahmoud-Yasser-16 <mahmoud.yasser@corporatica.com>
2021-11-12 22:22:58 +0200

    fix sorter

--- Task3/mp-topic-up-sort/mapper.py
+++ Task3/mp-topic-up-sort/mapper.py
@@ -5,7 +5,7 @@ import nltk
 import json
 from topic_list import extract_topic
 for line in sys.stdin:
-    if nltk.pos_tag([line.split(" ")[0]])[0][1] == "NN":
+    if nltk.pos_tag([line.split("\t")[0].replace("__up__","").replace("__down__","")])[0][1] == "NN":
         print(line)
 
 
